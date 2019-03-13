48f8c9f51277e36aa15aa182d7d54a56b733d1fe
George <dexor2@yahoo.com>
2019-03-13 17:42:29 +0100

    Basic converter

---
+++ currency_conv/currency/views.py
@@ -0,0 +1,20 @@
+import requests
+from django.shortcuts import render
+
+def index(request):
+	url = 'https://openexchangerates.org/api/latest.json?app_id=08282bcf2e894509a5a4faa5a46b4838'
+
+	# value = '1 US'
+
+	r = requests.get(url.format()).json()
+
+	conversion = {
+		# 'value': value,
+		'mkd': r['rates']['MKD'],
+		'rsd': r['rates']['RSD'],
+	}
+	# Optional: This line prints the value in the terminal console
+	# print(conversion) 
+	
+	data = {'conversion': conversion}
+	return render(request, 'currency/index.html', data)
